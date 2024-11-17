cf9ca98a85ca721d55ac52dcdf62fe9c2a5ddd28
Michel_Correa <devcafe096@gmail.com>
2024-11-17 16:42:05 -0300

    pokedex_challenge_1.0

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: './', 
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'; // Importe o módulo 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@/': path.resolve(__dirname, './src'), // Alias para a pasta 'src'
-    },
-  },
-});
+  base: './', 
+})
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'; // Importe o módulo 'path'
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './',
+//   resolve: {
+//     alias: {
+//       '@/': path.resolve(__dirname, './src'), // Alias para a pasta 'src'
+//     },
+//   },
+// });
